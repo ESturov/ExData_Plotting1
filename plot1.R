54dc884aac094f5f79b0c11aa637fbd8c7133ec0
ESturov <45300503+ESturov@users.noreply.github.com>
2018-12-23 12:52:05 +1300

    Add files via upload

---
+++ plot1.R
@@ -0,0 +1,9 @@
+house_data <-read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?")
+house_a_data <-rbind(house_data[house_data$Date=="1/2/2007",],house_data[house_data$Date=="2/2/2007",])
+
+
+house_a_data$Date <- as.Date(house_a_data$Date,"%d/%m/%Y")
+house_a_data<-cbind(house_a_data, "DateTime" = as.POSIXct(paste(house_a_data$Date, house_a_data$Time)))
+
+
+hist(as.numeric(house_a_data$Global_active_power), col="Red", main="Global Active Power", xlab="Global Active power (kilowatts)", ylab="Frequency")
